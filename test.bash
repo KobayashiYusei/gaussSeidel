2ed595a4dfc7b4b1c325c8c7a70611c2ec60052c
KobayashiYusei <s23C1054AF@s.chibakoudai.jp>
2024-11-30 07:39:50 +0900

    Add command

--- test.bash
+++ test.bash
@@ -1,20 +1,36 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2024 Kobayashi Yusei
+# SPDX-License-Identifier: BSD-3-Clause
+# See LICENSE file for more details.
 
+SCORE_FILE="score.txt"
+
+if [ -f "$SCORE_FILE" ]; then
+  rm "$SCORE_FILE"
+fi
 ng (){
-	echo ${1}行目が違うよ
-	res=1
+ echo "in line ${1} an Error occured."
+ res=1
 }
-
 res=0
-
 out=$(seq 1000 | ./numberguesser)
 [ "$?" = 0 ] || ng "$LINENO"
+echo "$out" | grep -q "Close!" || ng "$LINENO"
+echo "$out" | grep -q "Very Close!" || ng "$LINENO"
 
 out=$(echo | ./numberguesser)
 [ "$?" = 0 ] || ng "$LINENO"
+echo "$out" | grep -q "input number" || ng "$LINENO"
 
 out=$(echo "a" | ./numberguesser)
 [ "$?" = 0 ] || ng "$LINENO"
+echo "$out" | grep -q "input number" || ng "$LINENO"
 
-[ "$res" = 0 ] && echo OK
+out=$(echo "-1" | ./numberguesser)
+[ "$?" = 0 ] || ng "$LINENO"
+echo "$out" | grep -q "input number" || ng "$LINENO"
+
+[ -f "$SCORE_FILE" ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo PASS
 exit $res
